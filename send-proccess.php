b227d26d5221f87785d389931e1171947aef8310
GilbertoASJ <alvesgilberto84@gmail.com>
2021-04-16 11:18:33 -0300

    :sparkles: Updating project

--- send-proccess.php
+++ send-proccess.php
@@ -51,10 +51,14 @@
 		die();
 	}
 
-	// -------------------------------------- PHPMailer lib ----------------------------------------------- //
+	// -------------------------------------- PHPMailer lib ---------------------------------------------- //
 	$mail = new PHPMailer(true);
 
 	try {
+		// Acentuações
+		$mail->CharSet = 'UTF-8';
+		$mail->Encoding = 'base64';
+
 	    //Server settings
 	    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
 	    $mail->isSMTP();                                            //Send using SMTP
@@ -64,20 +68,20 @@
 	    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 
 	    ## Coloque nas duas linhas a seguir seu email e senha:
-	    $mail->Username   = 'teste@gmail.com';            //SMTP username
-	    $mail->Password   = 'teste_senha_123';                       //SMTP password
+	    $mail->Username   = 'email@teste';            //SMTP username
+	    $mail->Password   = 'senha_teste';                       //SMTP password
 	    $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 	    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 	    //Recipients
 	    ## Aqui informamos o remetente
-	    $mail->setFrom('teste_remetente@gmail.com', 'teste remetente');
+	    $mail->setFrom('email@teste');
 
 	    ## Aqui informamos o destinatário
-	    $mail->addAddress('teste_destinatario@gmail.com', 'teste destinatário');   //Add a recipient
+	    $mail->addAddress($mensagem->__get('para'));   				//Add a recipient
 
 	    ## Pode-se adicionar quantos destinatários forem precisos
-	    // $mail->addAddress('ellen@example.com');               				//Name is optional
+	    // $mail->addAddress('ellen@example.com');               	//Name is optional
 
 	    ## Caso o destinatário responda o e-mail, a resposta será enviada por padrão para:
 	    // $mail->addReplyTo('info@example.com', 'Information');
@@ -95,21 +99,23 @@
 	    $mail->isHTML(true);                                  //Set email format to HTML
 
 	    ## Assunto do e-mail:
-	    $mail->Subject = 'Oi eu sou o assunto do email';
+	    $mail->Subject = $mensagem->__get('assunto');
 
 	    ## Mensagem principal do email, com suporte a tags html.
-	    $mail->Body    = 'Olá, eu sou o conteúdo principal do <strong>e-mail</strong>.';
+	    $mail->Body    = $mensagem->__get('mensagem');
 
 	    ## Mensagem principal do email, sem suporte a tags html.
-	    $mail->AltBody = 'Olá, eu sou o conteúdo principal do e-mail.';
+	    $mail->AltBody = $mensagem->__get('mensagem');
 
 	    $mail->send();
-	    echo 'Message has been sent';
+	    echo 'E-mail enviado com sucesso.';
 
 	} catch (Exception $e) {
 	    echo "Não foi possível enviar esse e-mail, tente novamente mais tarde.
 	     	Detalhes do erro: {$mail->ErrorInfo}";
 	}
+
+	// ------------------------------------ / PHPMailer lib ---------------------------------------------- //
 
 	// echo "<pre>";
 	// 	print_r($mensagem);
